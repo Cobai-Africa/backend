24bac05398f93409f5f5c9d116422397dead67ff
shieldstring <edmunddaniels06@gmail.com>
2022-11-10 01:01:25 +0100

    added province to order models

--- models/orderModel.js
+++ models/orderModel.js
@@ -19,6 +19,7 @@ const orderSchema = new mongoose.Schema(
       fullName: { type: String, required: true },
       address: { type: String, required: true },
       city: { type: String, required: true },
+      province: { type: String, required: true },
       postalCode: { type: String, required: true },
       country: { type: String, required: true },
       lat: Number,
